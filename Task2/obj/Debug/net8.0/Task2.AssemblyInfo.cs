b140439db4f75f625994e7d15dfe856f7feb013d
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-03-13 15:24:29 +0300

    complete task

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92d50b3b2a936108c338d2dcfacde6a17daf15fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d012ae99fefaf9429127bb062db36c4d56a426")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
