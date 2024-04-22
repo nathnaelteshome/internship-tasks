ca1f96d8b1d977207f34beca251314d5759ed97c
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-04-22 10:03:08 +0300

    Update AssemblyInfoInputs.cache and AssemblyInfo.cs files

--- BlogApp/src/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ BlogApp/src/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bac36dd78f4554259cee0b0cae66161fb25d20b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+512d3a295c2227adb748da570dbd47954c6aa83e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BlogApp/src/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ BlogApp/src/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258e0164bd7eafd3f76d2915ef502fb29ea84e39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+512d3a295c2227adb748da570dbd47954c6aa83e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
